9b48c394a83d3eb2c328d2c85a27d21c4bcb69bc
Soumyadip Ghosh <soumyadipghosh220114@gmail.com>
2019-03-06 23:59:05 +0530

    Google Vision Added

---
+++ Google_Cloud_Vision/main.py
@@ -0,0 +1,21 @@
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision import types
+
+client = vision.ImageAnnotatorClient()
+
+file_name = os.path.join(
+    os.path.dirname(__file__),'img.jpg')
+
+
+with io.open(file_name,'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+response = client.label_detection(image=image)
+labels = response.label_annotations
+print("Labels :")
+for label in labels:
+    print(label.description)
