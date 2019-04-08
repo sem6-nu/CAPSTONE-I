51c35789493f1710fb1b132e422aec62e040d92e
Rahul Ghosh <ghrahul2016@gmail.com>
2019-04-09 03:49:52 +0530

    cnntest in master branch

---
+++ CNN/cnntest.py
@@ -0,0 +1,18 @@
+import unittest
+from predict_image import result
+
+
+class Tests(unittest.TestCase):
+
+    def test_1(self):
+        """Check that cat1 is cat"""
+        self.assertEqual(result('cat1.jpg'),'Cat')
+
+    def test_2(self):
+        """check that dog3 is dog"""
+        self.assertEqual(result('dog2.jpg'), 'Dog') 
+
+    
+
+if __name__ == "__main__":
+unittest.main()
