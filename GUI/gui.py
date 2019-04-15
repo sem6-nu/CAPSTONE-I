590da64326fe5553a928a679b26132e2e21baf25
samirghouri <samira.ghouri@st.niituniversity.in>
2019-04-15 13:14:18 +0530

    GUI for own model

--- GUI/gui.py
+++ GUI/gui.py
@@ -14,9 +14,6 @@ def mfileopen():
     # file_label = tk.Label(text=file1).grid(column=2, row=3)
 
 
-file_label = tk.Label(text="Step 1:").grid(column=2, row=1)
-button = tk.Button(text="open file ", command=mfileopen).grid(column=3, row=1)
-
 # ----LABEL1----
 label1 = tk.Label(text="TensorFlow", font=('Comic Sans MS', 30))
 label1.grid(column=0, row=0, padx=100, pady=100)
@@ -37,9 +34,22 @@ label3.grid(column=2, row=0, padx=100, pady=100)
 
 
 # ----BUTTON3----
+file_label = tk.Label(text="Step 1:").grid(column=2, row=1)
+button = tk.Button(text="open file ", command=mfileopen).grid(column=3, row=1)
 label_button3 = tk.Label(text="Step 2 :").grid(column=2, row=2)
 button3 = tk.Button(text="Click me")
 button3.grid(column=3, row=2)
+
+# ----LABEL4----
+label4 = tk.Label(text="Own Model", font=('Comic Sans MS', 30))
+label4.grid(column=4, row=0, padx=100, pady=100)
+
+# ----BUTTON4----
+file_label = tk.Label(text="Step 1:").grid(column=4, row=1)
+button = tk.Button(text="open file ", command=mfileopen).grid(column=5, row=1)
+label_button4 = tk.Label(text="Step 2 :").grid(column=4, row=2)
+button4 = tk.Button(text="Click")
+button4.grid(column=5, row=2)
 
 
 window.mainloop()
