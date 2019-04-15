336d3d7200d5e80da7555fb5299ca8eec577856c
Soumyadip Ghosh <soumyadipghosh220114@gmail.com>
2019-04-15 13:48:54 +0530

    update

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
+import tkinter as tk
+import os
+import inspect
+import pathlib
 
 window = tk.Tk()
 
@@ -11,22 +15,36 @@ window.geometry("400x400")
 
 def mfileopen():
     file1 = filedialog.askopenfile()
-    # file_label = tk.Label(text=file1).grid(column=2, row=3)
+    #return file1
+    file_label = tk.Label(text=file1).grid(column=2, row=3)
+
+def callYOLORealTime():
+    os.system(' python3 yolo-real-time.py --yolo yolo-coco')
+    
+    
+def callYOLO():
+    file1 = filedialog.askopenfile()
+    print(file1)
+    #os.system(' python3 yolo-real-time.py --yolo yolo-coco')
+
+def callTensor():
+    os.system('python3 test.py')
 
 
-# ----LABEL1----
+
+#----LABEL1----
 label1 = tk.Label(text="TensorFlow", font=('Comic Sans MS', 30))
 label1.grid(column=0, row=0, padx=100, pady=100)
 # ----BUTTON1----
-button1 = tk.Button(text="Click me")
+button1 = tk.Button(text="Click me",command = callTensor)
 button1.grid(column=0, row=1)
 
 # ----LABEL2----
-label2 = tk.Label(text="YOLO_RealTime", font=('Comic Sans MS', 30))
-label2.grid(column=1, row=0, padx=100, pady=100)
+label1 = tk.Label(text="YOLO_RealTime", font=('Comic Sans MS', 30))
+label1.grid(column=1, row=0, padx=100, pady=100)
 # ----BUTTON2----
-button2 = tk.Button(text="Click me")
-button2.grid(column=1, row=1)
+button1 = tk.Button(text="Click me",command = callYOLORealTime)
+button1.grid(column=1, row=1)
 
 # ----LABEL3----
 label3 = tk.Label(text="YOLO", font=('Comic Sans MS', 30))
@@ -35,7 +53,7 @@ label3.grid(column=2, row=0, padx=100, pady=100)
 
 # ----BUTTON3----
 file_label = tk.Label(text="Step 1:").grid(column=2, row=1)
-button = tk.Button(text="open file ", command=mfileopen).grid(column=3, row=1)
+button = tk.Button(text="open file ", command=callYOLO).grid(column=3, row=1)
 label_button3 = tk.Label(text="Step 2 :").grid(column=2, row=2)
 button3 = tk.Button(text="Click me")
 button3.grid(column=3, row=2)
